2c55567d2cce5e160bebc80aed5aeb4192d90600
Lean Albuquerque <lean.albuquerque@mail.uft.edu.br>
2025-07-03 12:15:21 -0300

    BROKEN: script
    The script is not working as intended, future bugfix required

---
+++ script.js
@@ -0,0 +1,34 @@
+
+fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+  .then(res => res.json())
+  .then(data => {
+    const cards = data.data
+    .filter(c => c.fname.toLowerCase().includes("Magician"))
+    .slice(0,5);
+
+    const ul = document.getElementById('card-list');
+    ul.innerHTML = '';
+
+    cards.forEach(card => {
+      const li = document.createElement('li');
+      li.className = 'splide__slide';
+      li.innerHTML = `
+        <img loading = "lazy" src="${card.card_images[0].image_url}" alt="${card.name}">
+        <h2>${card.name}</h2>
+      `;
+      ul.appendChild(li);
+    });
+
+    new Splide('#image-carousel', {
+      type: 'loop',
+      perPage: 3,
+      autoplay: false,
+      pagination: false,
+      interval: 3000,
+      pauseOnHover: true,
+    }).mount();
+  })
+  .catch(err => {
+    console.error("Erro ao carregar cartas:", err);
+  });
+
